3add7615c1df769cce82e46b9b9479c7d752b961
emrizaal <emrizal21@gmail.com>
2016-10-27 14:19:07 +0700

    1st commit

---
+++ app/controllers/videos_controller.rb
@@ -0,0 +1,35 @@
+class VideosController < ApplicationController
+	def index
+		@videos = Video.all
+	end
+
+	def new
+		@video = Video.new
+	end
+
+	def show
+		@video = Video.find(params[:id])
+	end
+
+	def create
+		@video = Video.new(video_params)
+
+		if @video.save
+			redirect_to videos_path, notice: "The video #{@video.name} has been uploaded."
+		else
+			render "new"
+		end
+	end
+
+	def destroy
+		@video = Video.find(params[:id])
+		#@video = current_user.videos.find(params[:id])
+		@video.destroy
+		redirect_to videos_path, notice:  "The video #{@video.name} has been deleted."
+	end
+
+	private
+	def video_params
+		params.require(:video).permit(:name, :attachment)
+	end
+end
